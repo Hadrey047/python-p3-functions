771dd5736a3569207848d41615197746ffcf51df
Olayanju Oluwadamilare <hadreytechsolutions@gmail.com>
2023-09-27 15:14:28 +0100

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello Programmer!")
 
-def greet(name):
-    pass
+def greet(Dare):
+    print(f"Hello, {Dare}")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
 
 def halve(number):
-    pass
+    print(number/2)
+
+
+
